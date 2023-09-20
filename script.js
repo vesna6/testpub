16b161b59d1dcd1c61d56ce2795817144a585833
Nsr <shayestehnia.n@gmail.com>
2023-09-20 19:10:12 +0330

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+const h = document.querySelector("h1");
+// console.log(h.firstChild.firstChild, h.firstChild.firstChild.nodeName);
+// console.log(h);
+const h2 = document.querySelector("h2");
+const radiobtn = document.querySelector("input");
+
+const primenums = function (n) {};
+
+console.log(primenums(19));
+
+function createElement(temp) {
+  const v = temp.split(",");
+  const body = document.body;
+  const el = 2;
+  return el;
+}
